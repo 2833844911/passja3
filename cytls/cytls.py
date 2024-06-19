7e3c4cab04f1fef781c9dfbce3a9fae578c7677e
My Name <my-name@chromium.org>
2024-06-19 19:25:51 +0800

    tls pass

---
+++ cytls/cytls.py
@@ -0,0 +1,89 @@
+import requests
+import json as jn
+from urllib.parse import urlencode
+ 
+
+
+def get(url, headers=None, proxies=None, params=None,cookies=None, timeout=40, http2=False,allow_redirects=True):
+    if params != None:
+        url = url.split("?")[0]
+        url = f'{url}?{urlencode(params)}'
+    
+    if headers==None:
+        headers = {}
+    
+    if proxies==None:
+        print("请必须带上代理ip!!!")
+        return
+    
+    if "http" in proxies:
+        proxies = proxies['http'].split('/')[-1].strip()
+    if "https" in proxies:
+        proxies = proxies['https'].split('/')[-1].strip()
+    if cookies != None:
+        headers["Cookie"] =  '; '.join([f'{k}={v}' for k, v in cookies.items()])
+    if allow_redirects == True:
+        cancdx = "0"
+    else:
+        cancdx = "1"
+    dt = {
+        "cancdx": cancdx,
+        "method": "GET",
+        "url": url,
+        "headers":headers,
+        "postData":"",
+        "proxy": proxies,
+        "http2": http2
+    }
+    print(proxies)
+    ud = "http://103.71.69.97:7890/request"
+    response = requests.post(ud, json=dt,timeout=timeout, allow_redirects=False)
+    return response
+
+
+
+def post(url, headers=None, proxies=None, params=None,cookies=None,json=None, data = None, timeout=40, http2=False,allow_redirects=True):
+    if params != None:
+        url = url.split("?")[0]
+        url = f'{url}?{urlencode(params)}'
+    
+    if headers==None:
+        headers = {}
+    
+    if proxies==None:
+        print("请必须带上代理ip!!! 必须https代理且不能有账号密码")
+        return
+    
+    if "http" in proxies:
+        proxies = proxies['http'].split('/')[-1].strip()
+    if "https" in proxies:
+        proxies = proxies['https'].split('/')[-1].strip()
+    if cookies != None:
+        headers["Cookie"] =  '; '.join([f'{k}={v}' for k, v in cookies.items()])
+    postData = ""
+    if data != None:
+        postData = data
+    if json != None:
+        postData = jn.dumps(json)
+        headers['Content-Type'] = 'application/json'
+
+    if allow_redirects == True:
+        cancdx = "0"
+    else:
+        cancdx = "1"
+    dt = {
+        "cancdx": cancdx,
+        "method": "POST",
+        "url": url,
+        "headers":headers,
+        "postData": postData,
+        "proxy": proxies
+    }
+    print(dt)
+    ud = "http://103.71.69.97:7890/request"
+    response = requests.post(ud, json=dt,timeout=timeout, allow_redirects=False)
+    return response
+
+
+
+
---
+++ test.py
@@ -0,0 +1,27 @@
+from cytls import cytls
+
+headers = {
+    "sec-ch-ua": '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+    "accept-language": 'en-US,en;q=0.9',
+    "upgrade-insecure-requests": "1",
+    "sec-ch-ua-mobile": "?0",
+    "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+    "accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    "priority": "u=0, i",
+    "sec-ch-ua-platform": '"Windows"',
+    "Sec-Fetch-Site": "same-origin",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Dest": "empty",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+}
+proxies = {"https": "http://user:passwd@255.255.255:8888"}
+
+url = "https://www.utmel.com/productdetail/texasinstruments-tps63802dlar-7758755"
+
+req = cytls.get(url, headers=headers, proxies=proxies, allow_redirects=True)
+
+
+dt = req.text
+print(req.status_code)
+print(req.headers)
+print(dt)
