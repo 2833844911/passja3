d89f087a736e31b1b67812337c8a5d8f6105ebcf
My Name <my-name@chromium.org>
2024-07-03 10:04:27 +0800

    tls pass

--- cytls/cytls.py
+++ cytls/cytls.py
@@ -1,65 +1,64 @@
 import requests
 import json as jn
 from urllib.parse import urlencode
- 
 
 
-def get(url, headers=None, proxies=None, params=None,cookies=None, timeout=40, http2=False,allow_redirects=True):
+def get(url, headers=None, proxies=None, params=None, cookies=None, timeout=40, http2=False, allow_redirects=True):
     if params != None:
         url = url.split("?")[0]
         url = f'{url}?{urlencode(params)}'
-    
-    if headers==None:
+
+    if headers == None:
         headers = {}
-    
-    if proxies==None:
+
+    if proxies == None:
         print("Please bring your proxy IP address!!!")
         return
-    
+
     if "http" in proxies:
         proxies = proxies['http'].split('/')[-1].strip()
     if "https" in proxies:
         proxies = proxies['https'].split('/')[-1].strip()
     if cookies != None:
-        headers["Cookie"] =  '; '.join([f'{k}={v}' for k, v in cookies.items()])
+        headers["Cookie"] = '; '.join([f'{k}={v}' for k, v in cookies.items()])
     if allow_redirects == True:
-        cancdx = "0"
-    else:
         cancdx = "1"
+    else:
+        cancdx = "0"
     dt = {
         "cancdx": cancdx,
         "method": "GET",
         "url": url,
-        "headers":headers,
-        "postData":"",
-        "proxy": proxies,
+        "headers": headers,
+        "timeout": timeout,
+        "postData": "",
+        "proxy": "http://"+proxies,
         "http2": http2
     }
-    print(proxies)
-    ud = "http://103.71.69.97:7890/request"
-    response = requests.post(ud, json=dt,timeout=timeout, allow_redirects=False)
+    ud = "http://103.71.69.97:28080/request"
+    response = requests.post(ud, json=dt, timeout=timeout, allow_redirects=False)
     return response
 
 
-
-def post(url, headers=None, proxies=None, params=None,cookies=None,json=None, data = None, timeout=40, http2=False,allow_redirects=True):
+def post(url, headers=None, proxies=None, params=None, cookies=None, json=None, data=None, timeout=40, http2=False,
+         allow_redirects=True):
     if params != None:
         url = url.split("?")[0]
         url = f'{url}?{urlencode(params)}'
-    
-    if headers==None:
+
+    if headers == None:
         headers = {}
-    
-    if proxies==None:
+
+    if proxies == None:
         print("Please bring your proxy IP address!!!")
         return
-    
+
     if "http" in proxies:
         proxies = proxies['http'].split('/')[-1].strip()
     if "https" in proxies:
         proxies = proxies['https'].split('/')[-1].strip()
     if cookies != None:
-        headers["Cookie"] =  '; '.join([f'{k}={v}' for k, v in cookies.items()])
+        headers["Cookie"] = '; '.join([f'{k}={v}' for k, v in cookies.items()])
     postData = ""
     if data != None:
         postData = data
@@ -68,22 +67,18 @@ def post(url, headers=None, proxies=None, params=None,cookies=None,json=None, da
         headers['Content-Type'] = 'application/json'
 
     if allow_redirects == True:
-        cancdx = "0"
-    else:
         cancdx = "1"
+    else:
+        cancdx = "0"
     dt = {
         "cancdx": cancdx,
         "method": "POST",
         "url": url,
-        "headers":headers,
+        "headers": headers,
         "postData": postData,
-        "proxy": proxies
+        "timeout": timeout,
+        "proxy": "http://"+proxies
     }
-    print(dt)
-    ud = "http://103.71.69.97:7890/request"
-    response = requests.post(ud, json=dt,timeout=timeout, allow_redirects=False)
+    ud = "http://103.71.69.97:28080/request"
+    response = requests.post(ud, json=dt, timeout=timeout, allow_redirects=False)
     return response
-
-
-
-
--- test.py
+++ test.py
@@ -1,24 +1,14 @@
 from cytls import cytls
 
-headers = {
-    "sec-ch-ua": '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
-    "accept-language": 'en-US,en;q=0.9',
-    "upgrade-insecure-requests": "1",
-    "sec-ch-ua-mobile": "?0",
-    "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
-    "accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    "priority": "u=0, i",
-    "sec-ch-ua-platform": '"Windows"',
-    "Sec-Fetch-Site": "same-origin",
-    "Sec-Fetch-Mode": "cors",
-    "Sec-Fetch-Dest": "empty",
-    "Accept-Encoding": "gzip, deflate, br, zstd",
+headers ={
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
 }
-proxies = {"https": "http://user:passwd@255.255.255:8888"}
 
-url = "https://www.utmel.com/productdetail/texasinstruments-tps63802dlar-7758755"
 
-req = cytls.get(url, headers=headers, proxies=proxies, allow_redirects=True)
+proxies =  {"https": "http://14.121.105.170:8008"}
+
+url ="https://www.baidu.com/"
+req = cytls.get(url, headers=headers, proxies=proxies)
 
 
 dt = req.text
