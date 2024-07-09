fc462279b4d11a9f12fae09c16b943a5dbf6c0b1
陈不不 <72036853+2833844911@users.noreply.github.com>
2024-07-09 12:44:24 +0800

    更新cookies

--- cytls/cytls.py
+++ cytls/cytls.py
@@ -1,6 +1,7 @@
 import requests
 import json as jn
 from urllib.parse import urlencode
+import http.cookies
 import urllib
 ud =  "http://103.71.69.97:28080/request"
 def get(url, headers=None, proxies=None, params=None, cookies=None, timeout=40, http2=False, allow_redirects=True):
@@ -37,6 +38,18 @@ def get(url, headers=None, proxies=None, params=None, cookies=None, timeout=40, 
     }
 
     response = requests.post(ud, json=dt, timeout=timeout, allow_redirects=False)
+    set_cookies = response.headers.get('Set-Cookie')
+    if set_cookies:
+        cookies = set_cookies.split(', ')
+        parsed_cookies = http.cookies.SimpleCookie()
+
+        for cookie in cookies:
+            if 'path' in cookie:
+                end_pos = cookie.find(';') + 1
+                single_cookie_str = cookie[:end_pos].strip()
+                parsed_cookies.load(single_cookie_str)
+        response.cookies = parsed_cookies
+
     return response
 
 
@@ -82,4 +95,15 @@ def post(url, headers=None, proxies=None, params=None, cookies=None, json=None, 
         "proxy": "http://"+proxies
     }
     response = requests.post(ud, json=dt, timeout=timeout, allow_redirects=False)
+    set_cookies = response.headers.get('Set-Cookie')
+    if set_cookies:
+        cookies = set_cookies.split(', ')
+        parsed_cookies = http.cookies.SimpleCookie()
+
+        for cookie in cookies:
+            if 'expires' in cookie:
+                end_pos = cookie.find(';') + 1
+                single_cookie_str = cookie[:end_pos].strip()
+                parsed_cookies.load(single_cookie_str)
+        response.cookies = parsed_cookies
     return response
